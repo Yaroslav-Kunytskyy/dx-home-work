fb1e025a803e9b116d5cf1c4ae953726924fc7f0
Yaroslav Kunytskyy <yaroslav_kunytskyy@pecodesoftware.com>
2022-07-12 12:38:38 +0300

    refactored task

--- app.ts
+++ app.ts
@@ -1,49 +1,54 @@
 const inputStr =
   "“Smart people learn from everything and everyone, average people from their experience, stupid people already, have all the answers” (Socrates)";
 
+function getWordsArr(string: string): string[] {
+  const filterRegex = /[^a-z]/gi;
+
+  return string.trim().split(filterRegex).filter(Boolean);
+}
+
 function filterLongestString(stringArr: string[]): string[] {
-  const stringSortedByLength = stringArr.sort((a, b) => b.length - a.length);
+  let results: string[] = [];
 
-  const longestString: string[] = [];
-  let previousWordLength = stringSortedByLength[0].length;
-
-  for (let i = 0; i < stringSortedByLength.length; i++) {
-    if (previousWordLength > stringSortedByLength[i].length) {
-      break;
+  stringArr.forEach((string) => {
+    if (!results[0] || string.length > results[0].length) {
+      results = [string];
     }
 
-    longestString.push(stringSortedByLength[i]);
-  }
+    if (string.length === results[0].length) {
+      results.push(string);
+    }
+  });
 
-  return longestString;
+  return results;
+}
+
+function getStringWithMostVowels(stringArr: string[]): string {
+  const nonVowelsRegex = /[^aeiou]/gi;
+  const stringsVowelsOnly = stringArr.map((s) => s.replace(nonVowelsRegex, ""));
+
+  let result = "";
+  let biggestLength = 0;
+
+  stringsVowelsOnly.forEach((string, i) => {
+    if (string.length > biggestLength) {
+      result = stringArr[i];
+      biggestLength = string.length;
+    }
+  });
+
+  return result;
 }
 
 function getLongestWord(string: string) {
-  const trimmedString = string.trim();
-
-  const filterRegex = /[a-z]| /gi;
-  const matchingSymbolArray = trimmedString.match(filterRegex);
-
-  if (!matchingSymbolArray) {
-    throw new Error("String did not match '/[a-z]| /gi' regex pattern.");
-  }
-
-  const filteredString = matchingSymbolArray.join("");
-  const stringArray = filteredString.split(" ");
-  const longestWords = filterLongestString(stringArray);
+  const alphabetCharacters = getWordsArr(string);
+  const longestWords = filterLongestString(alphabetCharacters);
 
   if (longestWords.length === 1) {
     return longestWords[0];
   }
 
-  const vowelsRegex = /[aeiou]/gi;
-
-  const sortedByVowelLength = longestWords.sort(
-    (a, b) =>
-      (b.match(vowelsRegex)?.length || 0) - (a.match(vowelsRegex)?.length || 0)
-  );
-
-  return sortedByVowelLength[0];
+  return getStringWithMostVowels(longestWords);
 }
 
 console.log(getLongestWord(inputStr));
